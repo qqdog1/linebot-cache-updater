3bb381e172545d46a5ad07f0c2f8a3db66a66a03
qqdog1 <qqdog1@yahoo.com.tw>
2020-03-01 11:46:30 +0800

    1. cache exception and log

--- src/main/java/name/qd/linebot/updater/task/TaskManager.java
+++ src/main/java/name/qd/linebot/updater/task/TaskManager.java
@@ -3,9 +3,13 @@ package name.qd.linebot.updater.task;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import name.qd.linebot.updater.CacheSender;
 
 public class TaskManager {
+	private Logger log = LoggerFactory.getLogger(TaskManager.class);
 	private static TaskManager instance = new TaskManager();
 	private Set<CacheUpdateTask> set = new HashSet<>();
 	private CacheSender cacheSender = new CacheSender();
@@ -21,7 +25,11 @@ public class TaskManager {
 		for(CacheUpdateTask task : set) {
 			task.doTask();
 			String jsonString = task.getJsonString();
-			cacheSender.send(jsonString);
+			try {
+				cacheSender.send(jsonString);
+			} catch (Exception e) {
+				log.error("do {} task failed.", task.getClass().getSimpleName(), e);
+			}
 		}
 	}
 }
